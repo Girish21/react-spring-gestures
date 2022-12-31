fc6de1f9cc8480dd395cd217311101be1e372c6e
Girish21 <girishkumarv21@gmail.com>
2022-12-31 22:24:06 +0530

    strip backrgound gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       width: {
         'app-content': 'min(100%, 100vw - 30rem)',
       },
+      backgroundImage: {
+        'color-strip':
+          'linear-gradient(to right, red 0%, #ff0 17%, lime 33%, cyan 50%, blue 66%, #f0f 83%, red 100%)',
+      },
     },
   },
   plugins: [],
