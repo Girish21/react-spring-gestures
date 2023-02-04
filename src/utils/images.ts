11f8bf9e10b813d743ec2b7bedd9b1065e5644de
Girish21 <girishkumarv21@gmail.com>
2023-02-04 19:37:28 +0530

    extract images

---
+++ src/utils/images.ts
@@ -0,0 +1,7 @@
+export const IMAGES = [
+  'https://images.unsplash.com/photo-1669751553048-d78e452fcb55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  'https://images.unsplash.com/photo-1670700880082-e3c577fa248a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
+  'https://images.unsplash.com/photo-1670813347701-352337009210?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
+  'https://images.unsplash.com/photo-1670979313222-310eaa2d5601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  'https://images.unsplash.com/photo-1670711454039-9c666bda8240?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+] as const
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './clsx'
 export * from './clamp'
 export * from './svg'
+export * from './images'
