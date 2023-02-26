ef5157dcff2b4f97eda84b21a17232ae61b12f0e
Girish21 <girishkumarv21@gmail.com>
2023-02-26 19:29:37 +0530

    better layout animation on the indicator

--- src/pages/simple-drag-indicator/page.tsx
+++ src/pages/simple-drag-indicator/page.tsx
@@ -1,7 +1,8 @@
 import {
-  useSpring,
   animated,
+  to,
   useIsomorphicLayoutEffect,
+  useSpring,
 } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
 import * as React from 'react'
@@ -10,40 +11,63 @@ import { getItemId, useItemRefs } from '../../hooks/use-items-ref'
 import { clamp } from '../../utils'
 
 function SimpleDragIndicator() {
+  const nextIdxRef = React.useRef(1)
   const currentIdx = React.useRef(1)
   const { getItemProps, itemsRefs } = useItemRefs()
   const [style, api] = useSpring(() => ({
-    width: 0,
+    c: 1,
     x: 0,
+    width: 0,
     config: { tension: 300, friction: 60 },
   }))
 
   const bind = useDrag(
     ({ active, direction: [x], movement: [mx], distance: [dx], cancel }) => {
-      if (active && dx > 60) {
-        const nextIdx = clamp(
-          x < 0 ? currentIdx.current + 1 : currentIdx.current - 1,
-          1,
-          3,
-        )
-        currentIdx.current = nextIdx
+      const nextIndex = x < 0 ? currentIdx.current + 1 : currentIdx.current - 1
+
+      if (active && dx > 80) {
+        const nextIdx = clamp(nextIndex, 1, 3)
+        nextIdxRef.current = nextIdx
+
         cancel()
       }
+
+      const optimisticNextIndex = clamp(nextIndex, 1, 3)
+      const optimisticNextEl = itemsRefs.current.get(
+        getItemId(optimisticNextIndex),
+      )
+
       const activeEl = itemsRefs.current.get(getItemId(currentIdx.current))
-      const activeElBoundingRect = activeEl?.getBoundingClientRect()
+
+      const lerp = (floor: number, ceil: number, ratio: number) =>
+        floor * (1 - ratio) + ceil * ratio
+
+      const fraction = active
+        ? dx /
+          Math.abs(
+            (optimisticNextEl?.offsetLeft ?? 0) - (activeEl?.offsetLeft ?? 0),
+          )
+        : 0
 
       api.start({
-        width: activeElBoundingRect?.width ?? 0,
+        width: lerp(
+          activeEl?.offsetWidth ?? 0,
+          optimisticNextEl?.offsetWidth ?? 0,
+          fraction,
+        ),
         x: (activeEl?.offsetLeft ?? 0) - (active ? mx : 0),
+        onResolve: () => {
+          currentIdx.current = nextIdxRef.current
+        },
       })
     },
   )
 
   useIsomorphicLayoutEffect(() => {
     const activeEl = itemsRefs.current.get(getItemId(currentIdx.current))
-    const activeElBoundingRect = activeEl?.getBoundingClientRect()
+
     api.set({
-      width: activeElBoundingRect?.width ?? 0,
+      width: activeEl?.offsetWidth ?? 0,
       x: activeEl?.offsetLeft ?? 0,
     })
   }, [])
@@ -56,7 +80,10 @@ function SimpleDragIndicator() {
         <div {...getItemProps({ index: 3 })}>Tab Very Long Name 3</div>
         <animated.div
           className='absolute left-0 bottom-0 h-0.5 bg-red-600'
-          style={style}
+          style={{
+            x: style.x,
+            width: style.width,
+          }}
         />
       </div>
       <div
